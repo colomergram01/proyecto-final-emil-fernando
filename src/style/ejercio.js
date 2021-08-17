2b6380537c505cd459a673837e1af89071106935
colomergram01 <89094061+colomergram01@users.noreply.github.com>
2021-08-17 10:36:31 -0600

    Add files via upload

---
+++ src/style/ejercio.js
@@ -0,0 +1,10 @@
+function ocultar(){
+    document.getElementById('obj1').style.display = 'none';
+    }
+
+
+    function mostrar(){
+        document.getElementById('obj2').style.display = 'block';
+        }
+
+        
